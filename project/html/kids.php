14eea312e740b8e6d294058445bf369ae0bafb2f
MatoBebej <m.bebej6@gmail.com>
2025-03-25 11:00:54 +0100

    added empty pages which we will work with

--- project/html/kids.php
+++ project/html/kids.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+</head>
+<body>
+<h1>KIds</h1>
+</body>
+</html>
