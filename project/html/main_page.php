779e51ad701989a8964121b6de039251be79cced
kurcinamarian <marian.kurcina2003@gmail.com>
2025-03-25 10:48:05 +0100

    added navbar

--- project/html/main_page.php
+++ project/html/main_page.php
@@ -1,11 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Page</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <style>
+        .navbar {
+            background-color: #D9D9D9;
+        }
+        .top-container {
+            background-color: #F3F3F3;
+        }
+    </style>
+
+
+
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<div class="container-fluid top-container py-2">
+    <div class="container d-flex justify-content-between align-items-center">
+        <a class="text-dark text-decoration-none fw-bold" href="#">FTW</a>
+        <div>
+            <a class="text-dark text-decoration-none" href="#">Register</a>
+            <span class="mx-2 text-dark">|</span>
+            <a class="text-dark text-decoration-none" href="#">Login</a>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#"><span style="font-size: 2rem;">&#9917;</span> </a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item"><a class="nav-link ms-4" href="#" style="font-size: 1.5rem; font-weight: bold;">MEN</a></li>
+                <li class="nav-item"><a class="nav-link ms-4" href="#" style="font-size: 1.5rem; font-weight: bold;">WOMEN</a></li>
+                <li class="nav-item"><a class="nav-link ms-4" href="#" style="font-size: 1.5rem; font-weight: bold;">KIDS</a></li>
+            </ul>
+            <a href="#" class="me-3 text-dark placeholder-box"><i class="fas fa-magnifying-glass"></i></a>
+            <a href="#" class="me-3 text-dark placeholder-box"><i class="fas fa-heart"></i></a>
+            <a href="#" class="me-3 text-dark placeholder-box"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+    </div>
+</nav>
+
+
+</ul>
+
+
+
+
 </body>
 </html>
